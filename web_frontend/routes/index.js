03b9cf753f9ef8be5316a004de5e39c5765805ab
Doug Erkkila <derkkila@gmail.com>
2018-08-06 16:00:12 -0400

    item info get update
    Move item retrieval from Node router to Jade template

--- web_frontend/routes/index.js
+++ web_frontend/routes/index.js
@@ -25,48 +25,8 @@ router.get('/shop/cart', function(req, res, next) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
-      var product=JSON.parse(data);
-      var total=0;
-
-      var Promise = require('promise');
-
-
-      console.log(product)
-
-        var prom = new Promise(function(resolve, reject) {
-          var price_total=0;
-          product.forEach((product)=>{
-          // Do async job
-          http.get('http://localhost:6767/products/'+product.product_id, (resp) => {
-            let data = '';
-
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-              data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-              var product_details=JSON.parse(data);
-              price_total+=product.qty*product_details.price;
-              console.log(price_total);
-            });
-
-          }).on("error", (err) => {
-            console.log("Error: " + err.message);
-            reject(err);
-          });
-        });
-        resolve(price_total);
-  }).then(function(result) {
-  total+=result;
-  console.log("Final Total: "+total);
-  res.render('cart_view', { title: "Cart", total: total });
-  // Use user details from here
-  console.log(userDetails)
-}, function(err) {
-  console.log(err);
-});
+      var cart=JSON.parse(data);
+      res.render('cart_view', { title: "Cart", cart: cart })
     });
 
   }).on("error", (err) => {
