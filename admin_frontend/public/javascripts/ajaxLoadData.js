5b530075ba07eabf78597af3850309d5fbdb1836
Doug Erkkila <derkkila@gmail.com>
2018-08-31 10:12:46 -0400

    Add orders to Admin
    Add order list to Admin panel

--- admin_frontend/public/javascripts/ajaxLoadData.js
+++ admin_frontend/public/javascripts/ajaxLoadData.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 
-	ajaxGet();
+	productGet();
+	orderGet();
 
 	// DO GET
-	function ajaxGet(){
+	function productGet(){
 		$.ajax({
 			type : "GET",
 			url : "http://"+window.location.hostname+"/products/",
@@ -27,6 +28,35 @@ $(document).ready(function() {
 
 				$( "#productList tbody tr:odd" ).addClass("info");
 				$( "#productList tbody tr:even" ).addClass("success");
+			},
+			error : function(e) {
+				alert("ERROR: ", e);
+				console.log("ERROR: ", e);
+			}
+		});
+	}
+	function orderGet(){
+		$.ajax({
+			type : "GET",
+			url : "http://"+window.location.hostname+"/orders/",
+			success: function(result){
+				$.each(result, function(i, order){
+
+					var orderRow = '<tr>' +
+                    '<td>' + order.id + '</td>' +
+                    '<td>' + order.user_id + '</td>' +
+                    '<td>' + order.total + '</td>' +
+                    '<td>' + order.qty + '</td>' +
+                    '<td><a href="/admin/edit_product?id='+order.id+'">Edit</a></td>' +
+                    '<td><a href="http://'+window.location.hostname+'/orders/delete/'+order.id+'">Delete</a></td>' +
+                    '</tr>';
+
+					$('#orderList tbody').append(orderRow);
+
+		        });
+
+				$( "#orderList tbody tr:odd" ).addClass("info");
+				$( "#orderList tbody tr:even" ).addClass("success");
 			},
 			error : function(e) {
 				alert("ERROR: ", e);
