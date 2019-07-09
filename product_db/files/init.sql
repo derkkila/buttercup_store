74b1d9359799ab4b98f5cda2be06824937bee28d
Doug Erkkila <derkkila@splunk.com>
2019-07-09 11:18:21 -0400

    Update product_db/files/init.sql
    Add K8s Monitoring Book to Init

--- product_db/files/init.sql
+++ product_db/files/init.sql
@@ -21,6 +21,16 @@ This e-book gives you a deep dive into what collectd is and how you can begin in
 <li>Analyze collectd data</li>
 <li>Use Splunk and collectd together to analyze large amounts of infrastructure data</li>", "book","how-to",9.99,1000);
 
+INSERT INTO product_list (name,description,type,category,price,qty)
+VALUES ("A Beginner's Guide to Kubernetes Monitoring","Application container technology, like Kubernetes, is revolutionizing app development, bringing previously unimagined flexibility and efficiency to the development process. But a good container monitoring solution is still necessary for dynamic container-based environments to unify container data with other infrastructure data — only then can you have better contextualization and root cause analysis.<br /><br />
+  In this guide, we'll define the importance and benefits of software container monitoring in container vendors today — like Docker, Kubernetes, RedHat OpenShift, or Amazon EKS. Learn what a typical and successful Kubernetes deployment looks like and how to effectively monitor its orchestration.<br /><br />
+  Download your copy of A Beginner's Guide to Kubernetes Monitoring to learn:<br />
+  <ul>
+  <li>About containers and their benefits</li>
+  <li>How to deploy Kubernetes</li>
+  <li>How to monitor Kubernetes deployments</li>
+  </ul>","book","education",11.99,10000);
+
 CREATE TABLE product_images
 (
 id INTEGER,
@@ -30,3 +40,6 @@ filepath TEXT
 
 INSERT INTO product_images (id,imgname,filepath)
 VALUES (1,"primary_image","/wstatic/images/a-beginners-guide-to-collectd.png");
+
+INSERT INTO product_images (id,imgname,filepath)
+VALUES (2,"primary_image","/wstatic/images/a-beginners-guide-to-collectd.png");
